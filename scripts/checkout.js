8119722090d387eda06cb7ed6bbddac304200484
NATNAEL GETACHEW ASEFA <natigech35@gmail.com>
2025-04-14 17:57:54 -0400

    deleting html from the page when clicking delete

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -16,7 +16,9 @@ cart.forEach((cartItem) => {
   });
 
   cartSummaryHtml += `
-    <div class="cart-item-container">
+    <div class="cart-item-container js-cart-item-container-${
+      matchingProduct.id
+    }">
       <div class="delivery-date">Delivery date: Wednesday, June 15</div>
 
       <div class="cart-item-details-grid">
@@ -92,6 +94,9 @@ document.querySelectorAll(".js-delete-link").forEach((link) => {
   link.addEventListener("click", () => {
     const productId = link.dataset.productId;
     removeFromCart(productId);
-    console.log(cart);
+    const container = document.querySelector(
+      `.js-cart-item-container-${productId}`
+    );
+    container.remove();
   });
 });
