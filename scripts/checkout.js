199e43ee2bfe96825a49531c097d79af5412f985
NATNAEL GETACHEW ASEFA <natigech35@gmail.com>
2025-04-16 10:46:40 -0400

    delivery update functions works

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,4 @@
-import { cart, removeFromCart } from "../data/cart.js";
+import { cart, removeFromCart, updateDeliveryOption } from "../data/cart.js";
 import { products } from "../data/products.js";
 import { formatCurrency } from "./utils/money.js";
 import dayjs from "https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js";
@@ -79,9 +79,11 @@ function deliveryOptionsHTML(matchingProduct, cartItem) {
         ? "FREE"
         : `${formatCurrency(deliveryOption.priceCents)}-`;
 
-    const isChecked = deliveryOption.id === cartItem.deliveryOptionId;
+    const isChecked = deliveryOption.id === cartItem.deliveryOptionsId;
 
-    html += ` <div class="delivery-option">
+    html += ` <div class="delivery-option js-delivery-option" data-product-id="${
+      matchingProduct.id
+    }"  data-delivery-option-id="${deliveryOption.id}">
             <input
               type="radio"
               ${isChecked ? "checked" : ""}
@@ -109,5 +111,12 @@ document.querySelectorAll(".js-delete-link").forEach((link) => {
       `.js-cart-item-container-${productId}`
     );
     container.remove();
+  });
+});
+
+document.querySelectorAll(".js-delivery-option").forEach((element) => {
+  element.addEventListener("click", () => {
+    const { productId, deliveryOptionId } = element.dataset;
+    updateDeliveryOption(productId, deliveryOptionId);
   });
 });
