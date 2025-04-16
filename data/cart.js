f0862c0eca9975ce5ed42f6346f412cc570a5eb4
NATNAEL GETACHEW ASEFA <natigech35@gmail.com>
2025-04-16 15:36:49 -0400

    orderSummary works

--- data/cart.js
+++ data/cart.js
@@ -5,12 +5,12 @@ if (!cart) {
     {
       productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
       quantity: 2,
-      deliveryOptionsId: "1",
+      deliveryOptionId: "1",
     },
     {
       productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
       quantity: 1,
-      deliveryOptionsId: "2",
+      deliveryOptionId: "2",
     },
   ];
 }
@@ -27,31 +27,35 @@ export function addToCart(productId) {
       matchingItem = cartItem;
     }
   });
+
   if (matchingItem) {
     matchingItem.quantity += 1;
   } else {
     cart.push({
       productId: productId,
       quantity: 1,
-      deliveryOptionsID: "1",
+      deliveryOptionId: "1",
     });
   }
+
   saveToStorage();
 }
 
 export function removeFromCart(productId) {
   const newCart = [];
+
   cart.forEach((cartItem) => {
     if (cartItem.productId !== productId) {
       newCart.push(cartItem);
     }
   });
+
   cart = newCart;
 
   saveToStorage();
 }
 
-export function updateDeliveryOption(productId, deliveryOptionsId) {
+export function updateDeliveryOption(productId, deliveryOptionId) {
   let matchingItem;
 
   cart.forEach((cartItem) => {
@@ -60,6 +64,7 @@ export function updateDeliveryOption(productId, deliveryOptionsId) {
     }
   });
 
-  matchingItem.deliveryOptionsId = deliveryOptionsId;
+  matchingItem.deliveryOptionId = deliveryOptionId;
+
   saveToStorage();
 }
